6c94717f8d3d84a4e154545c25e958efaff40015
mitri-dvp <mitri.dvp@gmail.com>
2023-08-10 13:14:23 -0500

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
+import "~/styles/font.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,46 @@
 import { type Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "primary-semi-cond": [
+          "URW DIN SemiCond",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        "primary-cond": ["URW DIN Cond", ...defaultTheme.fontFamily.sans],
+        primary: ["URW DIN", ...defaultTheme.fontFamily.sans],
+        "owners-wide": ["OwmersWide", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        "4.5xl": ["44px", "32px"],
+        "5.5xl": ["52px", "32px"],
+        "10xl": ["144px", "32px"],
+        "11xl": ["160px", "32px"],
+      },
+      backgroundImage: {
+        button: "url('/assets/svg/button-pattern.svg')",
+        "button-steam": "url('/assets/svg/button-pattern-steam.svg')",
+        "button-discord": "url('/assets/svg/button-pattern-discord.svg')",
+        "ready-email-input": "url('/assets/svg/ready-email-input.svg')",
+        "corporate-gunn":
+          "url('/assets/png/corporate-card-gunn-background.png')",
+        "corporate-vector":
+          "url('/assets/png/corporate-card-vector-background.png')",
+        "corporate-shift":
+          "url('/assets/png/corporate-card-shift-background.png')",
+      },
+      colors: {
+        green: {
+          primary: "#31e78b",
+        },
+      },
+      screens: {
+        "2xl": "1440px",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
